6be975a930b6360cd0b70faf71d83f040ef3c691
Zac Bauermeister <zac@bauermeister.com>
2023-01-17 17:39:15 -0800

    initial commit

---
+++ OuterWildsRandomSpeedrun/OuterWildsRandomSpeedrun.cs
@@ -0,0 +1,29 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace OuterWildsRandomSpeedrun
+{
+    public class OuterWildsRandomSpeedrun : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(OuterWildsRandomSpeedrun)} is loaded!", MessageType.Success);
+
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+}
