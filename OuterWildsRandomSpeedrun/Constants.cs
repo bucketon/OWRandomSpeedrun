49aa706e8e5ab2ce09e014aa9c4ee972f99c0e1f
Chris Maines <cheetos1@gmail.com>
2023-02-15 00:17:14 -0800

    Changing constants class to struct

--- OuterWildsRandomSpeedrun/Constants.cs
+++ OuterWildsRandomSpeedrun/Constants.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace OuterWildsRandomSpeedrun
 {
-  public class Constants
+  public struct Constants
   {
     public static Color OW_ORANGE_COLOR = new Color(0.968f, 0.498f, 0.207f);
     public static Color OW_SELECTED_COLOR = new Color(0.9882f, 0.8627f, 0.7686f);
