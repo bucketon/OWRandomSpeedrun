70ef7110e41d13b2ad29dfcaf9a8bcfa633923cc
Chris Maines <cheetos1@gmail.com>
2023-02-23 21:25:54 -0800

    Addressing PR comments

--- NomaiGrandPrix/SpawnActionFactory.cs
+++ NomaiGrandPrix/SpawnActionFactory.cs
@@ -1,39 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 namespace NomaiGrandPrix
 {
-  public class SpawnActionFactory
-  {
-    private static SpawnActionFactory Instance = new SpawnActionFactory();
-
-
-    private SpawnActionFactory()
+    public class SpawnActionFactory
     {
-      // Private constructor
-    }
+        private static SpawnActionFactory Instance = new SpawnActionFactory();
+        private static Dictionary<string, Action[]> actionsMap = new Dictionary<string, Action[]>
+        {
+            {"Spawn_Module_Sunken", new Action[] { OpenSunkenModuleAirlock }}
+        };
+        
+        private SpawnActionFactory()
+        {
+            // Private constructor
+        }
 
-    public static Action[] GetActionsForSpawn(string spawnId)
-    {
-      if (spawnId == null)
-      {
-        throw new InvalidOperationException("Spawn ID for getting actions cannot be null");
-      }
-      
-      switch (spawnId)
-      {
-        case "Spawn_Module_Sunken":
-          return new Action[] { OpenSunkenModuleAirlock };
-        default:
-          return new Action[0];
-      }
+        public static Action[] GetActionsForSpawn(string spawnId)
+        {
+            if (spawnId == null)
+            {
+                throw new InvalidOperationException("Spawn ID for getting actions cannot be null");
+            }
+            
+            return actionsMap.GetValueOrDefault(spawnId, new Action[0]);
+        }
 
+        private static void OpenSunkenModuleAirlock()
+        {
+            var giantsDeep = Locator._giantsDeep;
+            var airlock = giantsDeep.GetComponentInChildren<NomaiAirlock>();
+            var position = airlock._closeSwitches[0].transform.position;
+            airlock._listInterfaceOrb[0].SetOrbPosition(position);
+        }
     }
-    private static void OpenSunkenModuleAirlock()
-    {
-      var giantsDeep = Locator._giantsDeep;
-      var airlock = giantsDeep.GetComponentInChildren<NomaiAirlock>();
-      var position = airlock._closeSwitches[0].transform.position;
-      airlock._listInterfaceOrb[0].SetOrbPosition(position);
-    }
-  }
 }
